291dce3ef07d6e65787f0ee3a07d876fd79a46f1
2133649586 <54879522+2133649586@users.noreply.github.com>
2020-10-30 18:30:37 +0800

    Update function_dataset.py

--- function_dataset.py
+++ function_dataset.py
@@ -1,9 +1,18 @@
+
+import random
+import matplotlib.pyplot as plt
+
 dataset_y = []
 output = "dataset_y.txt"
 
 files = open("dataset_x.txt","r")
 for file in files:
     dataset_y.append(float(file)*3+6)
+
+print(dataset_y)
+
+plt.hist(dataset_y, bins=40, normed=0, facecolor="blue", edgecolor="black", alpha=0.7)
+plt.show()
 
 with open(output, 'w') as output_file:
     for i in dataset_y:
