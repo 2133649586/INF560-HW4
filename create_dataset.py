d29f9f4531e80713ccee972631830066057d95b9
2133649586 <54879522+2133649586@users.noreply.github.com>
2020-10-30 22:27:16 +0800

    Update create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -6,7 +6,7 @@ output = "dataset_x.txt"
 for i in range(0, 1000):
     num_100 = random.randint(1, 10000)
     dataset_x.append(num_100 / 100)
-    
+print(dataset_x)   
 plt.hist(dataset_x, bins=40, facecolor="blue", edgecolor="black", alpha=0.7)
 plt.show()
 
